d2b87b6453cb70016a317f958184d177e07620aa
jaykaran123 <jaydkaran2006@gmail.com>
2025-11-29 01:00:38 +0530

    Add prime factorization function in j9.py

---
+++ j9.py
@@ -0,0 +1,16 @@
+def factorize(n):
+    factors = []
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+num = int(input("Enter a number: "))
+factors = factorize(num)
+print(f"Prime factors: {' * '.join(map(str, factors))}")
